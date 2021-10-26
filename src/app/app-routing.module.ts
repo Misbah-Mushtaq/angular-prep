7990aeb52c552c917613b190ac04f90079628a4c
Misbah Mushtaq <misbahmushtaq75@gmail.com>
2021-10-26 15:38:54 +0500

    wildCardRoute - Redirecting route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path : "", redirectTo: '/employees', pathMatch: 'full'},
   {path : 'employees', component: EmployeeListComponent},
   {path : 'departments', component: DepartmentListComponent},
   {path : 'employeeDetails', component: EmployeeDetailsComponent},
+  {path : "**", component: PageNotFoundComponent},
 ];
 
 @NgModule({
@@ -16,4 +19,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [DepartmentListComponent, EmployeeListComponent, EmployeeDetailsComponent]
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent, EmployeeDetailsComponent, PageNotFoundComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,20 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './first-component/first-component.component';
 import { EmployeeService } from 'src/employee.service';
 import {HttpClientModule} from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    routingComponents
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
