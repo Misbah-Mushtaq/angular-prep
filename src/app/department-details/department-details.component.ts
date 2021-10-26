a7065374dacc48bd10decf7e736b5d546e20ceb8
Misbah Mushtaq <misbahmushtaq75@gmail.com>
2021-10-26 17:53:16 +0500

    Router snapshot ParamMap

--- src/app/department-details/department-details.component.ts
+++ src/app/department-details/department-details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-department-details',
   templateUrl: './department-details.component.html',
   styleUrls: ['./department-details.component.sass']
 })
 export class DepartmentDetailsComponent implements OnInit {
-
-  constructor() { }
+  public departmentId:any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id = parseInt(this.route.snapshot.paramMap.get('id')!)
+    this.departmentId = id;
   }
 
 }
