ce87129fd49bce897e2dfca05bd45e1bab54b467
Misbah Mushtaq <misbahmushtaq75@gmail.com>
2021-10-27 09:49:28 +0500

    WIP on Routing and Navigation - Handle navigation from prev next

--- src/app/department-details/department-details.component.ts
+++ src/app/department-details/department-details.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, ParamMap, Router } from '@angular/router'
 @Component({
   selector: 'app-department-details',
   templateUrl: './department-details.component.html',
   styleUrls: ['./department-details.component.sass']
 })
 export class DepartmentDetailsComponent implements OnInit {
-  public departmentId:any;
-  constructor(private route: ActivatedRoute) { }
+  public departmentId: any
+  constructor (private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {
-    let id = parseInt(this.route.snapshot.paramMap.get('id')!)
-    this.departmentId = id;
+  ngOnInit (): void {
+    // let id = parseInt(this.route.snapshot.paramMap.get('id')!)
+    // this.departmentId = id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id')!)
+      this.departmentId = id
+    })
   }
 
+  goPrev (): void {
+    let prevId = this.departmentId - 1
+    this.router.navigate(['/departmentDetails', prevId])
+  }
+
+  goNext (): void {
+    let nextId = this.departmentId + 1
+    this.router.navigate(['/departmentDetails', nextId])
+  }
 }
