7790a6d14ac3af8805d0a003609ea3abaff1967f
Misbah Mushtaq <misbahmushtaq75@gmail.com>
2021-11-03 17:14:52 +0500

    WIP Angular Forms : Form validation conditional classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms'
 import { AppRoutingModule, routingComponents } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { FirstComponentComponent } from './first-component/first-component.component'
 import { HttpClientModule } from '@angular/common/http'
 import { DepartmentContactComponent } from './department-contact/department-contact.component'
 import { TdfPracticeComponent } from './tdf-practice/tdf-practice.component'
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +15,7 @@ import { TdfPracticeComponent } from './tdf-practice/tdf-practice.component'
     DepartmentContactComponent,
     TdfPracticeComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
