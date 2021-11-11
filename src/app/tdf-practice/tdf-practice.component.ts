3e44eb4d596f4321d0faee450f3e364bb0bae9c5
Misbah Mushtaq <misbahmushtaq75@gmail.com>
2021-11-11 14:52:34 +0500

    TDF - Testing comit

--- src/app/tdf-practice/tdf-practice.component.ts
+++ src/app/tdf-practice/tdf-practice.component.ts
@@ -8,15 +8,21 @@ import { User } from '../user'
 })
 export class TdfPracticeComponent implements OnInit {
   topics = ['Angular', 'Bootstrap', 'React', 'Javascript', 'Vue']
+  topicHasError = true;
   userModal = new User(
     'Misbah',
     'misbah@gmail.com',
     '08966736731',
-    '',
+    'default',
     'morning',
     true
   )
+  validateTopic(value:any){
+    if(value==="default"){
+      this.topicHasError = true
+    }else this.topicHasError = false
+  }
   constructor () {}
-
+  
   ngOnInit (): void {}
 }
